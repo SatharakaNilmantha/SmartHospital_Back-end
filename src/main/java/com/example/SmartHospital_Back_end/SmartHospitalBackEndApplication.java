3cf2ca8debd483b21bd1441178c16a46c65ef616
Satharaka Nilmantha <158058129+SatharakaNilmantha@users.noreply.github.com>
2025-01-31 23:54:34 +0530

    Create the smarthospital back-end folder

---
+++ src/main/java/com/example/SmartHospital_Back_end/SmartHospitalBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.SmartHospital_Back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHospitalBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartHospitalBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SmartHospital_Back_end/SmartHospitalBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SmartHospital_Back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartHospitalBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
